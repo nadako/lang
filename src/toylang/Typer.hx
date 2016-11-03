3fac85e55263c32f7e6ab6d44a8a4f744f17d2d2
Dan Korostelev <nadako@gmail.com>
2016-11-03 04:01:06 +0300

    unify function types \o/

--- src/toylang/Typer.hx
+++ src/toylang/Typer.hx
@@ -372,6 +372,16 @@ class Typer {
                     }
                 }
                 result;
+            case [TFun(aargs, aret), TFun(bargs, bret)] if (aargs.length == bargs.length):
+                if (!unify(aret, bret)) { // unify a with b - return types are covariant
+                    false;
+                } else {
+                    for (i in 0...aargs.length) {
+                        if (!unify(bargs[i].type, aargs[i].type)) // unify b with a - argument types are contravariant
+                            return false;
+                    }
+                    true;
+                }
             default:
                 false;
         }
