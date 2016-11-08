b37a316f232cd2ccbcca75bab2c7ad3bd5f63264
Dan Korostelev <nadako@gmail.com>
2016-11-08 15:22:58 +0300

    add web demo because why not

---
+++ demo/Web.hx
@@ -0,0 +1,31 @@
+import js.Browser.document;
+import js.html.Element;
+import js.html.TextAreaElement;
+
+@:expose
+class Web {
+    static var code:TextAreaElement = cast document.getElementById("code");
+    static var result:Element = cast document.getElementById("result");
+
+    static function main(editor) {
+        var model = editor.getModel();
+        model.onDidChangeContent(function(_) {
+            try {
+                var input = byte.ByteData.ofString(model.getValue());
+                var parser = new toylang.Parser(input, "code");
+                var typer = new toylang.Typer();
+
+                var decls = parser.parse();
+                var r = [];
+                for (decl in decls) {
+                    var typedDecl = typer.typeDecl(decl);
+                    r.push(toylang.Dump.dumpTypeDecl(typedDecl));
+                }
+
+                result.innerText = r.join("\n\n");
+            } catch (e:Any) {
+                result.innerText = 'ERROR: $e';
+            }
+        });
+    }
+}
